980ef54c0aa90c08b78a1b604ba1345d8bf29d31
Dustin Walker <me@dustinwalker.com>
2024-08-13 10:18:01 -0500

    ADD: boilerplate

--- config/vite.php
+++ config/vite.php
@@ -2,13 +2,22 @@
 
 use craft\helpers\App;
 
+
+// Decides which port to use for devServerPublic. This allows
+// you to visit the http or https port. If you plant to only
+// use http, make sure that PRIMARY_SITE_URL is set to http
+//
+// Match ports to .ddev/config.yaml -> web_extra_exposed_ports
+$host = Craft::$app->getRequest()->getIsConsoleRequest()
+    ? App::env('PRIMARY_SITE_URL')
+    : Craft::$app->getRequest()->getHostInfo();
+$httpPort = 3000;
+$httpsPort = 3001;
+$devServerPort = str_starts_with($host, 'https') ? $httpsPort : $httpPort;
+
 return [
-    'checkDevServer' => true,
-    'devServerInternal' => 'http://localhost:3000',
-    'devServerPublic' => App::env('PRIMARY_SITE_URL') .  ':3000',
-    'errorEntry' => 'src/js/app.js',
-    'cacheKeySuffix' => '',
-    'manifestPath' => Craft::getAlias('@webroot') . '/dist/manifest.json',
-    'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
-    'useDevServer' => (bool) App::env('VITE_USE_DEV_SERVER'),
+    'devServerPublic' => "$host:$devServerPort",
+    'serverPublic' => '/dist/',
+    'useDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
 ];
--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,3 @@
-/* CSS */
-import "@css/app.pcss";
+import '../css/app.css'
 
-/**
- * Alpine JS
- * Best to put it last so that all other potential JS is available
- * when components start getting initialized.
- */
-import Alpine from 'alpinejs';
-window.Alpine = Alpine;
-Alpine.start();
-
-/**
- * Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
- */
-if (import.meta.hot) {
-  import.meta.hot.accept(() => {
-    console.log("HMR");
-  });
-}
+console.warn('Hello Craft CMS Starter')
